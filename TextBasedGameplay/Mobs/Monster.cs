31a889352cab00912af295d3eb6fcb751b7e513c
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2021-01-14 16:35:57 +0100

    Used autoproperties for Class Monster, added virtual method Describe() and created empty subclasses

--- TextBasedGameplay/Mobs/Monster.cs
+++ TextBasedGameplay/Mobs/Monster.cs
@@ -6,5 +6,22 @@ namespace TextBasedGameplay.Mobs
 {
     class Monster
     {
+        public string Name { get; }
+        public int HealthPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Damage { get; set; }
+
+        public Monster(string name, int healthPoints, int experiencePoints, int damage)
+        {
+            Name = name;
+            HealthPoints = healthPoints;
+            ExperiencePoints = experiencePoints;
+            Damage = damage;
+        }
+
+        public virtual string Describe()
+        {
+            return $"A random {Name} appeared!";
+        }
     }
 }
